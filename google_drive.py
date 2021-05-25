4db09ccecd259e682b507ad2f5a0cadc2d58b7d7
Sergey Kulazhenko <sergeykulazhenko@Sergeys-MacBook-Pro.local>
2021-05-25 17:06:52 +0300

    “”

---
+++ google_drive.py
@@ -0,0 +1,22 @@
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+drive = GoogleDrive(gauth)
+
+
+# View all folders and file in your Google Drive
+fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file in fileList:
+  print('Title: %s, ID: %s' % (file['title'], file['id']))
+  # Get the folder ID that you want
+  if(file['title'] == "To Share"):
+      fileID = file['id']
+
+file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
+file1.SetContentFile("small_file.csv")
+file1.Upload() # Upload the file.
+print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))
