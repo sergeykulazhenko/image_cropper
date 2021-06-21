084a262d01de13ab646d1848458927f0f9e1b6ee
Sergey Kulazhenko <sergeykulazhenko@Sergeys-MacBook-Pro.local>
2021-06-21 21:42:40 +0300

    ftp7

--- img_cropper.py
+++ img_cropper.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_cropper import st_cropper
 from PIL import Image
-from pick_save import pick_random_img, load_file_trough_ftp
+from pick_save import pick_random_img, load_file_trough_ftp, save_cropped_img_ftp
 import random
 import os
 
@@ -29,5 +29,6 @@ cropped_img = st_cropper(img, realtime_update=True, box_color='#0000FF', aspect_
 st.image(cropped_img)
 
 if crop_button:
-  os.makedirs(os.path.dirname(base_folder_crop + "/" + str(img_file)), exist_ok=True)
-  cropped_img.save(base_folder_crop + "/" + str(img_file))
+  save_cropped_img_ftp(cropped_img, img_file):
+  #os.makedirs(os.path.dirname(base_folder_crop + "/" + str(img_file)), exist_ok=True)
+  #cropped_img.save(base_folder_crop + "/" + str(img_file))
