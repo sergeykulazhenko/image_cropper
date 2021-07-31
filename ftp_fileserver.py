6018a791b6af3dbe40ee3ebb89220bf01a291cda
Sergey Kulazhenko <sergeykulazhenko@Sergeys-MacBook-Pro.local>
2021-08-01 00:52:12 +0300

    skip file final

--- ftp_fileserver.py
+++
@@ -1,8 +0,0 @@
-import ftplib
-
-host = 'ftp4.nska.net'
-ftp_user = 'test1@lookies.by'
-ftp_password = 'U4hq0oLn'
-ftp = ftplib.FTP(host, ftp_user, ftp_password)
-directory = ftp.nlst('img600x866/A')
-print(directory[2:])
